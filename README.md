fe874037d134c890898ea8d4855224ea8df0d3be
Ken Ng <ng.ken84@gmail.com>
2018-09-03 15:48:37 -0700

    Add second alert the is not cancellable

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # AndroidAlertDialog
 
 Implements a simple example of Alert Dialogs from Android App Development Course on Udemy.
+
+## Types of Alerts
+1. Simple Alert
+2. Simple ALert that can't be cancelled or dismissed by tapping outside the alert.
--- app/src/main/java/com/example/ngken/androidalertdialogexample/MainActivity.java
+++ app/src/main/java/com/example/ngken/androidalertdialogexample/MainActivity.java
@@ -11,12 +11,14 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
 
     private Button btnAlert;
+    private Button uncancelAlert;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         setUpAlertButton();
+        setUpUncancelDialog();
     }
 
     private void setUpAlertButton() {
@@ -42,5 +44,35 @@ public class MainActivity extends AppCompatActivity {
                 myDialog.show();
             }
         });
+    }
+
+    private void setUpUncancelDialog() {
+        uncancelAlert = findViewById(R.id.uncancelAlertBtn);
+        uncancelAlert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder alert = new AlertDialog.Builder(MainActivity.this);
+
+
+                alert.setTitle("Alert Title");
+                alert.setMessage("Let's Go Somewhere");
+                alert.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Toast.makeText(getApplicationContext(), "OK Let's go!", Toast.LENGTH_SHORT).show();
+                    }
+                });
+                alert.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Toast.makeText(getApplicationContext(), "OK poo-face", Toast.LENGTH_SHORT).show();
+                    }
+                });
+                AlertDialog created = alert.create();
+                created.setCancelable(false);
+                created.show();
+            }
+        });
+
     }
 }
