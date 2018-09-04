b561f6738f965370285f3e33df12e31c411981df
Ken Ng <ng.ken84@gmail.com>
2018-09-03 20:09:08 -0700

    Implement Some functionality to Alert Dialog

--- app/src/main/java/com/example/ngken/androidalertdialogexample/CustomAlertActivity.java
+++ app/src/main/java/com/example/ngken/androidalertdialogexample/CustomAlertActivity.java
@@ -1,13 +1,58 @@
 package com.example.ngken.androidalertdialogexample;
 
+import android.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class CustomAlertActivity extends AppCompatActivity {
+
+    private Button customAlertButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_custom_alert);
+        setUpCustomAlertDialog();
+    }
+
+    private void setUpCustomAlertDialog() {
+        customAlertButton = findViewById(R.id.customAltBtn);
+        customAlertButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(CustomAlertActivity.this);
+                LayoutInflater inflater = LayoutInflater.from(CustomAlertActivity.this);
+
+                View myView = inflater.inflate(R.layout.dialog_custom_example, null);
+
+                dialogBuilder.setView(myView);
+                final AlertDialog dialog = dialogBuilder.create();
+
+                Button cancelButton = myView.findViewById(R.id.dce_cancel);
+                cancelButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        dialog.hide();
+                    }
+                });
+
+                final EditText editText = myView.findViewById(R.id.dce_name_field);
+                Button submitButton = myView.findViewById(R.id.dce_submit);
+                submitButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Toast.makeText(getApplicationContext(), "The name is " + editText.getText(), Toast.LENGTH_SHORT).show();
+                        dialog.hide();
+                    }
+                });
+
+                dialog.show();
+            }
+        });
     }
 }
